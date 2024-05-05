df0d76306c878c95b3b4f3564542f7cf8ff0660d
Watson-Dhrub <89378950+dhrub-dubey@users.noreply.github.com>
2024-05-05 21:12:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ function takeCommand(message) {
         window.open('Calculator:///');
         const finalText = "Opening Calculator";
         speak(finalText);
+    }  else if (message.includes('spotify')) {
+        window.open('Spotify:///');
+        const finalText = "Opening Spotify";
+        speak(finalText);
     } else {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "I found some information for " + message + " on Google";
